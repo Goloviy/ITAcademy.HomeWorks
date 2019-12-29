3d761ec81a0e21ad4cbcfed402d7ddc69b77589a
Kolxoznuk <goloviy@tut.by>
2019-12-29 18:15:54 +0300

    Домашка 7, задание 1.

---
+++ Solution/hm_07.Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace hm_07.Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите стихотворение разделяя строки точкой с запятой: ");
+            string txt = Console.ReadLine();
+            string[] array = txt.Split(';');           
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = array[i].Replace("O", "A");
+                Console.WriteLine(array[i]);
+            }   
+        }
+    }
+}

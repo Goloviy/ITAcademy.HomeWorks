aee7e444d1593896241c3e625d3dee8c5fb9955d
Kolxoznuk <goloviy@tut.by>
2020-01-06 00:00:13 +0300

    Домашка 8, задание 2.

---
+++ Solution/AssemblyOne/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AssemblyOne
+{
+    public class Employee
+    {
+        public const float numberPpublic = 3.14f;
+        private const float numberPprivate = 3.14f;
+        
+        internal string nameInternal;
+
+        public void SumPublic ()
+        {   
+          
+        }
+        private void SumPrivate()
+        {
+
+        }
+    }
+}

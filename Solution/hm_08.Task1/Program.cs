35cbf764fb2692212d6716cdbc42f0f92df943e4
Kolxoznuk <goloviy@tut.by>
2020-01-05 23:00:10 +0300

    Домашка 8, задание 1.

---
+++ Solution/hm_08.Task1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+namespace hm_08.Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string txt = "1a!2.3!!..4.!.? 6 7!.. ?";
+            int index = txt.IndexOf('?');
+            StringBuilder sb_01 = new StringBuilder(txt.Substring(0, index));
+            
+            for (int i = 0; i < sb_01.Length; i++)
+            {
+                if (sb_01[i] == '!' || sb_01[i] == '.')
+                {
+                    sb_01.Remove(i, 1);
+                    i--;
+                }
+            }        
+            
+            StringBuilder sb_02 = new StringBuilder(txt.Substring(index));
+            sb_02.Replace(' ', '_');
+            StringBuilder sb_03 = new StringBuilder();
+            sb_03.Append(sb_01); ;
+            sb_03.Append(sb_02);            
+            Console.WriteLine(sb_03);
+        }
+    }
+}

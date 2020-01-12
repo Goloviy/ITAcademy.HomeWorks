6fbac91e6384ba7098a6d42159fa51b762cbc219
Kolxoznuk <goloviy@tut.by>
2020-01-12 19:10:29 +0300

    Revert "Домашка 4, задание 1."
    This reverts commit 364747e7db8fee89f3a6e417cc099d8208d2679d.

---
+++ Solution/HW.02/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+namespace HW._02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*1. Чтение файла из E:\image.txt
+             *2. Запись полученных данных в массив.
+             *3. Создание массив байтов.
+             *4. Перевод полученных данных из файла в байты и запись их в массив.
+             *5. Конвертация байтов в пиксели и сохранение их в файле.
+             */
+            StreamReader textReader = new StreamReader(@"E:\image.txt", true);
+            string textReaderResult = textReader.ReadToEnd();
+
+
+            string[] arrayOfTextResult = textReaderResult.Split(' ');
+            byte[] imageBytes = new byte[arrayOfTextResult.Length - 1];
+
+            for (int i = 0; i < arrayOfTextResult.Length - 1; i++)
+            {
+                byte binary = Convert.ToByte(arrayOfTextResult[i], 2);
+                imageBytes[i] = binary;
+            }
+
+            File.WriteAllBytes(@"E:\image.png", imageBytes);
+            textReader.Dispose();
+        }
+    }
+}

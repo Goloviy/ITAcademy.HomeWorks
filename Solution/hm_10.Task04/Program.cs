33c71151d1198fe0a965be4711b7216af8c23dfe
Kolxoznuk <goloviy@tut.by>
2020-01-19 23:56:16 +0300

    Домашка 10, задание 4 и 5.

---
+++ Solution/hm_10.Task04/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace hm_10.Task04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //link https://www.codewars.com/kata/563089b9b7be03472d00002b
+            //Define a method/function that removes from a given array of integers all 
+            //the values contained in a second array.
+        }
+        public class Kata
+        {
+            public static int[] Remove(int[] integerList, int[] valuesList)
+            {
+                int a = integerList.Length;
+                for (int i = 0; i < integerList.Length; i++)
+                {
+                    for (int j = 0; j < valuesList.Length; j++)
+                    {
+                        if (integerList[i].Equals(valuesList[j]))
+                        {
+                            a--;
+                            integerList[i] = 0;
+                        }
+                    }
+                }
+
+                int[] result = new int[a];
+                int index = 0;
+                for (int i = 0; i < integerList.Length; i++)
+                {
+                    if (integerList[i] != 0)
+                    {
+                        result[index] = integerList[i];
+                        index++;
+                    }
+                }
+                return result;
+            }
+        }
+    }
+}
---
+++ Solution/hm_10.Task05/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace hm_10.Task05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //link - https://www.codewars.com/kata/54fe05c4762e2e3047000add
+            //Every time your spies see a new ship enter the dock, they will 
+            //create a new ship object based on their observations:
+            //draft - an estimate of the ship's weight based on how low it is in the water
+            // crew - the count of crew on board
+
+           // Taking into account that an average crew member on board adds 1.5 units to the draft, 
+           //a ship that has a draft of more than 20 without crew is considered worthy to loot. 
+           //Any ship weighing that much must have a lot of booty!
+        }
+        public class Ship
+        {
+            public int Draft;
+            public int Crew;
+
+            public Ship(int draft, int crew)
+            {
+                Draft = draft;
+                Crew = crew;
+            }
+            public bool IsWorthIt()
+            {
+                return (Draft - Crew * 1.5) > 20;
+            }
+
+        }
+    }
+}

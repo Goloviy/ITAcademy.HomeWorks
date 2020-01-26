3db35bc19f4bc680892770900a19e6140940f840
Kolxoznuk <goloviy@tut.by>
2020-01-26 21:29:01 +0300

    Домашка 11 и логи.

---
+++ Solution/hm11/Logger.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using log4net;
+using log4net.Config;
+
+namespace hm11
+{
+    public static class Logger
+    {
+
+        private static ILog log = LogManager.GetLogger(typeof(Logger));
+
+        public static ILog Log
+        {
+            get { return log; }
+        }
+
+        public static void InitLogger()
+        {
+            var logRepository = LogManager.GetRepository(Assembly.GetEntryAssembly());
+            XmlConfigurator.Configure(logRepository, new FileInfo("App.config"));
+        }
+    }
+}
--- Solution/hm11/Program.cs
+++ Solution/hm11/Program.cs
@@ -5,7 +5,10 @@ namespace hm11
     class Program
     {
         static void Main(string[] args)
-        {         
+        {
+            Logger.InitLogger();
+            //Logger.Log.Info($"Programm {System.Reflection.Assembly.GetExecutingAssembly().GetName().FullName} has been started");
+            Logger.Log.Info("Start program");
             static Figure GreateFigure (int index)
             {
                 int i = index;
@@ -14,29 +17,34 @@ namespace hm11
                 if (i == 0)
                 {
                     Circle circle = new Circle("Circle", rnd.Next(1,10));
+                    Logger.Log.Info("Greate circle");
                     return circle;
                 }
                 else if (i == 1)
                 {
                     Square square = new Square("Square", rnd.Next(1, 10));
+                    Logger.Log.Info("Greate square");
                     return square;
                 }
                 else if (i == 2)
                 {
                     Triangle triangle = new Triangle("Triangle", rnd.Next(1, 10));
+                    Logger.Log.Info("Greate triangle");
                     return triangle;
                 }
                 else return null;
             }        
-
+            
             Figure[] figures = new Figure[3];
 
             for (int i = 0; i < figures.Length; i++)
             {
                 figures[i] = GreateFigure(i);
+                Logger.Log.Info("Output to the console");
                 Console.WriteLine($"This is {figures[i].NameFigure}. CLR Type is {figures[i].ToString()}." +
                     $" Square is {figures[i].AreaFugure()}");
-            }            
+            }
+            Logger.Log.Info("Program completed successfullye");
         }
     }
 }
